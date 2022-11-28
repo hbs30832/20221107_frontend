bee86a2bd0a48011d5a66eaa9f27584fda5c4d40
hbs30832 <hbs9312@gmail.com>
2022-11-28 22:20:27 +0900

    20221128
    - JS 함수
    - JS 객체

--- 3_js/7_function.js
+++ 3_js/7_function.js
@@ -4,6 +4,7 @@
         - function 뒤에 함수의 이름을 작성하고 ()를 붙인다.
             => ()안에는 함수 실행에 필요한 값을 매개 변수(Parameter)로 전달.
             => 매개 변수는 여러개도 가능하고, 없어도 된다.
+            => 매개 변수를 통해서 전달되는 실제 값을 인자(Agrument)라고 한다.
         - 반환해줄 값이 있다면 return 뒤에 표현식을 작성한다.
             => 없다면 생략 가능. => undefined 반환
             => return은 함수의 종료를 의미한다. return 이후의 코드는 실행되지 않는다.
@@ -68,8 +69,84 @@ function getTotal(a, b, c) {
 }
 
 // 함수는 작은 단위로 쪼개서 사용하는 것이 좋다.
+console.log(total);
 var total = getTotal(3, 6, 9);
 var avg = divide(total, 3);
 console.log(avg);
 
 console.log(divide(getTotal(3, 6, 9), 3));
+
+// 기명 함수와 익명 함수
+
+/* 
+    기명 함수(Named Function) - 함수 선언식 
+        => 함수 호이스팅이 일어난다. => 함수 선언 이전에 호출해도 잘 작동한다. 구현부도 같이 호이스팅 된다.
+*/
+func();
+function func() {
+  console.log("기명 함수입니다.");
+}
+
+/* 
+    익명 함수(Anonymous Function) - 함수 표현식 
+        => 함수 호이스팅이 일어나지 않는다. 
+        => JavaScript는 함수를 값으로 취급한다. => 값으로써 함수를 전달할 때는 익명함수를 사용하면 간결하게 작성할 수 있다.
+*/
+// getData();
+let getData = function fetchData() {
+  console.log("익명함수입니다.");
+};
+
+/* 
+    화살표 함수(Arrow Function) - ES6(2015)
+        => function 키워드를 사용하지 않고 "=>" 를 통해서 간결하게 작성 가능하다. 
+        => 함수 표현식으로만 작성 가능하다.
+        => 중괄호와 return 키워드를 생략하면 표현식의 값을 return한다.
+            => 기본적으로 {}를 코드블록으로 인식한다.
+            => 반환값이 객체라면 ()안에 {}를 작성해야 return 생략이 가능하다.
+        => 다른 함수의 값으로 함수를 전달할 때 많이 사용한다.
+        => 함수 호이스팅 일어나지 않는다.
+        => this 바인딩이 일어나지 않는다.
+*/
+let add = (a, b) => {
+  return a + b;
+};
+
+let minus = (a, b) => a - b;
+
+console.log("2 - 1 : ", minus(3, 2));
+
+// 화살표 함수 이용해서 두 문자열을 연결하는 함수 만들어 보기
+// sumStrnig()
+let sumStrnig = (str1, str2) => str1 + str2;
+
+console.log(add("안녕하세요 ", "저는 황보석입니다."));
+console.log(sumStrnig("안녕하세요 ", "저는 황보석입니다."));
+
+let getObj = (age, name) => ({
+  age: age,
+  name: name,
+});
+
+let user = getObj(40, "seok");
+console.log(user);
+
+let globalFunc = function () {
+  console.log("외부 함부 실행");
+  let inner = function () {
+    console.log("내부 함수 실행");
+  };
+
+  inner();
+  console.log("외부 함수 종료");
+};
+
+globalFunc();
+
+function foo() {
+  return "foo";
+}
+
+// ()를 생략하면 함수 그 자체를 가리킨다. => 함수를 값으로 사용할 때는 호출이 아니라 식별자를 참조해야한다.
+let copyFunc = foo;
+console.log(copyFunc());
---
+++ 3_js/8_object.js
@@ -0,0 +1,75 @@
+/* 
+    객체(Object)
+        - key-value 형태로 여러 데이터를 저장할 수 있다.
+
+*/
+let user = {
+  age: 30,
+  name: {
+    firstName: "seok",
+    lastName: "hwangbo",
+  },
+};
+
+// 객체의 프로퍼티 값 참조
+console.log(user.name.firstName);
+
+// 객체의 프로퍼티 값 변경
+user.age = 31;
+console.log(user.age);
+
+// 객체에 새로운 프로퍼티 생성
+user.weight = 67;
+console.log(user);
+
+// 객체에 없는 프로퍼티에 접근하면 undefined. undefined에 다른 키값을 참조하려고 하면 오류 발생.
+// console.log(user.address.city);
+
+/* 
+    메서드(Method) : 객체의 프로퍼티가 함수라면 메서드라고 부른다.
+        => 객체.메서드()의 형식으로 호출한다.
+*/
+
+let dog = {
+  age: 10,
+  name: "coco",
+  bow: function () {
+    console.log("왈왈왈!");
+  },
+};
+dog.bow();
+
+/* 
+    this
+        - 함수 내부에서 this를 참조하면 함수를 호출한 객체를 가리킨다. => bow 함수에서 this를 참조하면 dog를 가리킨다.
+        - 객체 가지고 있는 고유의 정보를 이용하는 함수를 만들 수 있다.
+            => 같은 함수지만 호출하는 객체마다 결과가 달라진다.
+        - 메서드가 화살표 함수라면 this 바인딩이 안돼서 호출한 객체를 가리키지 않는다.
+            => this를 통해 참조할 값이 있다면 화살표 함수를 사용하면 안된다.
+*/
+
+let user02 = {
+  age: 30,
+  name: "seok",
+  introduce: () => {
+    console.log(this);
+    console.log(`안녕하세요 저는 ${this.age}세 ${this.name}입니다.`);
+  },
+};
+
+// this가 바인딩되는 대상을 바꿀 수 있다.
+user02.introduce.call({ name: "hwangbo" });
+
+user02.introduce();
+
+// triangle(삼각형) 객체에 너비와 높이를 프로퍼티 지정하고, getArea 넓이를 출력하는 메서드 만들기.
+let triangle = {
+  width: 20,
+  height: 10,
+  //  객체 메서드 함축 표현 => function 키워드 제외.
+  getArea() {
+    console.log((this.width * this.height) / 2);
+  },
+};
+
+triangle.getArea();
