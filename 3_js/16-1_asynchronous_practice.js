330c5f31f24bae1a1081bcb72aa2ea060ca0cd52
hbs30832 <hbs9312@gmail.com>
2022-12-08 19:41:09 +0900

    데이터 베이스

---
+++ 3_js/16-1_asynchronous_practice.js
@@ -0,0 +1,47 @@
+let movieList = [
+  {
+    id: 1,
+    title: "어벤저스",
+    director: 1,
+  },
+  {
+    id: 2,
+    title: "블랙펜서2",
+    director: 2,
+  },
+];
+
+let directorList = [
+  {
+    id: 1,
+    name: "안소니 루소",
+    movies: [
+      {
+        id: 1,
+        title: "어벤저스",
+        director: 1,
+      },
+      {
+        id: 3,
+        title: "그레이맨",
+        director: 1,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "라이언 쿠글러",
+    movies: [
+      {
+        id: 2,
+        title: "블랙펜서2",
+        director: 2,
+      },
+      {
+        id: 4,
+        title: "크리드",
+        director: 2,
+      },
+    ],
+  },
+];
